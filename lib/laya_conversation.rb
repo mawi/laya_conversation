d8f2d865b13171686a1b6d7a3fd66082663db530
Manuel Marín <mawitu@gmail.com>
2013-01-23 21:23:36 +0100

    Plugin triggers

--- lib/laya_conversation.rb
+++ lib/laya_conversation.rb
@@ -1,10 +1,11 @@
+# encoding: UTF-8
+
 require "laya_conversation/version"
 
 module LayaConversation
 
   class Config
     def self.init_data
-      binding.pry
       plugin = Plugin.find_or_create(:name => "laya_conversation", :description => "CONVERSATION BOT")
 
       plugin.keywords.find_or_create(:keyword => "manubot", :trigger => "fortune")
@@ -16,6 +17,27 @@ module LayaConversation
   end
 
   class Triggers
+    def self.woot(query)
+      woot = <<-EOF
+      ¯\\_(ツ)_/¯
+      EOF
+      woot
+    end
+
+    def self.fortune(query)
+      if query.include?("joke")
+        fortune = `fortune /usr/local/Cellar/fortune/9708/share/games/fortunes/humorix-misc`
+      else
+        fortune = `fortune -a`
+      end
+      "MANUBOT WISDOM: #{fortune}"
+    end
+
+    # def self.cleverbot(query)
+    #   message = query.gsub("mb: ","")
+    #   response = @cleverbot_client.write(message)
+    #   "MANUBOT says: #{response}"
+    # end
   end
 
 
